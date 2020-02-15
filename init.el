8427d9ecf24094fe6874caa413ea449667af64e4
MindfulProtons <17714502+MindfulProtons@users.noreply.github.com>
2020-02-15 16:28:27 -0500

    Whole config rewritten. Old code on old-config branch.

--- init.el
+++ init.el
@@ -1,24 +1,19 @@
-;; Package mirrors
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives             
+	'("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;; Customizations that don't like org-babel for some reason (??)
+;; customizations that don't play nice with org-babel
 
-;; disable scroll-bar
+;; scroll-bar and fringe mode disabled
 (scroll-bar-mode -1)
 (set-fringe-mode 0)
 
-;; End of weird customizations
+;; sets up use-package
 
-;; use-package installation
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
 ;; loads my config file from config.org
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-
-
-;; Stuff that emacs told me not to touch, so don't touch it!
