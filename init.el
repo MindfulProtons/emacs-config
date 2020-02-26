506f6bfa55440f384b4c8c432b24877bb77b87fa
MindfulProtons <mindfulprotons@outlook.com>
2020-02-26 15:01:27 -0500

    Added straight.el, fixed lsp-ui colors, and disabled package.el.

--- init.el
+++ init.el
@@ -1,19 +1,28 @@
-(require 'package)
-(add-to-list 'package-archives             
-	'("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
+;; bootstraps straight.el (https://github.com/raxod502/straight.el) and sets use-package up to use straight.
 
-;; customizations that don't play nice with org-babel
+(setq straight-use-package-by-default t)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq package-enable-at-startup nil)
 
 ;; scroll-bar and fringe mode disabled
 (scroll-bar-mode -1)
 (set-fringe-mode 0)
 
 ;; sets up use-package
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+(straight-use-package 'use-package)
 
 ;; loads my config file from config.org
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
