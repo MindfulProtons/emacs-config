96784bd46de6dee5ba3abe1860bc84baa862e4d3
MindfulProtons <17714502+MindfulProtons@users.noreply.github.com>
2022-07-03 14:18:39 -0400

    Changed font, LSP provider, and miscellaneous init changes.

--- early-init.el
+++ early-init.el
@@ -7,8 +7,15 @@
 (advice-add #'package--ensure-init-file :override #'ignore)
 
 ;; scroll-bar and fringe mode disabled
+(setq inhibit-startup-message t)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(mouse-color . "white") default-frame-alist)
 (scroll-bar-mode -1)
 (set-fringe-mode 0)
 
 ;; decrease startup time by stopping emacs from resizing the frame
 (setq frame-inhibit-implied-resize t)
+
+(customize-set-variable 'initial-major-mode 'fundamental-mode)
